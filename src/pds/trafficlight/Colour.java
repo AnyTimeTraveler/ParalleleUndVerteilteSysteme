1e9212d15e82bb2b80b86338bdc9ac98dcec9213
Simon Struck <simon@simonscode.org>
2021-03-21 22:44:56 +0100

    Add provided code

---
+++ src/pds/trafficlight/Colour.java
@@ -0,0 +1,45 @@
+/*
+ * Colour.java
+ *
+ * Copyright (c) 2021-2021 HS Emden-Leer
+ * All Rights Reserved.
+ *
+ * @version 1.00 - 17 Mar 2021 - GJV - initial version
+ */
+
+package pds.trafficlight;
+
+/**
+ * Representation of the colours of a traffic light.
+ * <br><code><b>[PVS-21SS]</b></code>
+ *
+ * @author Gert Veltink
+ * @version 1.00 - 17 Mar 2021
+ */
+public enum Colour {
+  /** the colour red. */
+  RED,
+  /** the colour green. */
+  GREEN,
+  /** the colour yellow. */
+  YELLOW;
+
+
+  /**
+   * Returns the colour of the next phase of a traffic light.
+   * Order: red - green - yellow - red - ...
+   *
+   * @param c   a colour
+   * @return    the colour of the next phase
+   */
+  public static Colour next(Colour c) {
+    switch (c) {
+      case RED:
+        return GREEN;
+      case GREEN:
+        return YELLOW;
+      default:
+        return RED; // catch all
+    }
+  }
+}
---
+++ src/pds/trafficlight/Reporter.java
@@ -0,0 +1,42 @@
+/*
+ * Reporter.java
+ *
+ * Copyright (c) 2021-2021 HS Emden/Leer
+ * All Rights Reserved.
+ *
+ * @version 1.10 - 18 Mar 2021 - GJV - added msg for debugging purposes
+ * @version 1.00 - 17 Mar 2021 - GJV - initial version
+ */
+
+package pds.trafficlight;
+
+/**
+ * A very simple reporter for the intersection.
+ * <br><code><b>[PVS-21SS]</b></code>
+ *
+ * @author Gert Veltink
+ * @version 1.10 - 18 Mar 2021
+ */
+public class Reporter {
+
+  /**
+   * Shows the cardinal direction and the colour of a traffic light.
+   *
+   * @param cd  the cardinal direction
+   * @param c   the colour
+   */
+  public static void show(final CardinalDirection cd, final Colour c) {
+    msg(cd + ":\t" + c);
+  }
+
+  /**
+   * Shows a general message.
+   *
+   * @param s   the message string
+   */
+  public static void msg(final String s) {
+    System.out.println(s);
+    System.out.flush();
+  }
+
+}
