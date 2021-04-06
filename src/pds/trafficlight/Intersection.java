cc7f3e4f729088c8abbc045a6815d3cc842ebecb
Mosaab <mosaab.helal@stud.hs-emden-leer.de>
2021-04-06 12:03:49 +0200

    Mosaab A2

--- src/pds/trafficlight/Intersection.java
+++ src/pds/trafficlight/Intersection.java
@@ -1,29 +1,39 @@
 package pds.trafficlight;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
 
-import static pds.trafficlight.CardinalDirection.NORTH;
-import static pds.trafficlight.CardinalDirection.next;
-
-import javax.swing.SwingUtilities;
-
-/**
- * Entrypoint.
- */
 public class Intersection {
 
-  /**
-   * Implements the main method for this class. Constructs the traffic lights and activates them
-   * (threads).
-   *
-   * @param args not used
-   */
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Gui());
-    CardinalDirection dir = NORTH;
-    final CardinalDirection startDirection = NORTH;
-    for (int i = 0; i < 4; i++) {
-      new TrafficLight(dir, startDirection).start();
-      dir = next(dir);
-    }
-  }
 
+  public static CardinalDirection sharedDirection = CardinalDirection.NORTH;
+
+
+  public volatile static Semaphore sem = new Semaphore(1);
+
+  public static List<TrafficLight> ampeln = new ArrayList<>();
+
+  public static List<Colour> farben = new ArrayList<>();
+
+
+  public static void main(String[] args) throws InterruptedException {
+    //creating threads:
+    CardinalDirection first = CardinalDirection.NORTH;
+    for (int i = 0; i < 4; i++) {
+      ampeln.add(new TrafficLight(first, CardinalDirection.opposite(first)));//thread created
+      farben.add(ampeln.get(i).colour);//Coulour init red
+      first = CardinalDirection.next(first);
+    }
+    for (int i = 0; i < 4; i++) {
+      ampeln.get(i).start();
+    }
+
+    Thread.sleep(10000);
+
+    for (int i = 0; i < 4; i++) {
+      ampeln.get(i).halt();
+    }
+
+
+  }
 }
