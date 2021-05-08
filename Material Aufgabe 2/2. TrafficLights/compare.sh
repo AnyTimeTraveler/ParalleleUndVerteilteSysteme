b3f46c7312f67841bb5e719002407481e3bff3e8
Simon Struck <simon@simonscode.org>
2021-05-08 17:15:49 +0200

    Fix compare script

--- Material Aufgabe 2/2. TrafficLights/compare.sh
+++ Material Aufgabe 2/2. TrafficLights/compare.sh
@@ -1,8 +1,8 @@
 for i in $(seq 1 4); do
   echo "Testing v${i} for equivalence:"
-  /usr/bin/mcrl22lps TrafficLights_v${i}.mcrl2 TrafficLights_v${i}_own.lps --quiet
-  /usr/bin/lps2lts TrafficLights_v${i}_own.lps TrafficLights_v${i}_own.lts --quiet
-  /usr/bin/ltsconvert TrafficLights_v${i}_own.lts TrafficLights_v${i}_own_bb.lts --equivalence=branching-bisim --quiet
-  /usr/bin/ltscompare TrafficLights_v${i}_own_bb.lts TrafficLights_v${i}.lts --counter-example --equivalence=branching-bisim
+  mcrl22lps TrafficLights_v${i}.mcrl2 TrafficLights_v${i}_own.lps --quiet
+  lps2lts TrafficLights_v${i}_own.lps TrafficLights_v${i}_own.lts --quiet
+  ltsconvert TrafficLights_v${i}_own.lts TrafficLights_v${i}_own_bb.lts --equivalence=branching-bisim --quiet
+  ltscompare TrafficLights_v${i}_own_bb.lts TrafficLights_v${i}.lts --counter-example --equivalence=branching-bisim
   echo
 done
